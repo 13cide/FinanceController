031f844dccb30015293dd3da075ebdfb3720bf1b
Aleksey Khlopkov <v.v.sidorovgg@gmail.com>
2023-03-14 21:26:23 +0300

    new navbar

---
+++ app/src/main/java/com/example/financecontroller/Fragments/CategoryFragment.java
@@ -0,0 +1,27 @@
+package com.example.financecontroller.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.financecontroller.R;
+
+
+public class CategoryFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_category, container, false);
+    }
+}
--- app/src/main/java/com/example/financecontroller/MainActivity.java
+++ app/src/main/java/com/example/financecontroller/MainActivity.java
@@ -7,8 +7,12 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
 
 import com.example.financecontroller.DataClasses.Localdb;
+import com.example.financecontroller.Fragments.CategoryFragment;
 import com.example.financecontroller.Fragments.StatisticFragment;
 import com.example.financecontroller.Fragments.TransactionsFragment;
 import com.example.financecontroller.databinding.ActivityMainBinding;
@@ -16,6 +20,8 @@ import com.example.financecontroller.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
+
+
     final private FragmentManager fragmentManager = getSupportFragmentManager();
 
     public static Localdb db = new Localdb();
@@ -30,29 +36,77 @@ public class MainActivity extends AppCompatActivity {
 
         swapFragment(new TransactionsFragment());
 
-//        binding.transactions.setOnClickListener(view -> {
-//            if (chosenFragment != 1) {
-//                binding.transactions.setImageResource(R.drawable.transactions_open);
-//                binding.statistic.setImageResource(R.drawable.statistic_close);
-//                swapFragment(new TransactionsFragment());
-//                chosenFragment = 1;
-//            }
-//        });
-//
-//        binding.statistic.setOnClickListener(view -> {
-//            if (chosenFragment != 2) {
-//                binding.transactions.setImageResource(R.drawable.transactions_close);
-//                binding.statistic.setImageResource(R.drawable.statistic_open);
-//                swapFragment(new StatisticFragment());
-//                chosenFragment = 2;
-//            }
-//        });
+        binding.historyLayout.setOnClickListener(view -> {
+            if (chosenFragment != 1) {
 
-        binding.addButton.setOnClickListener(view -> {
-            Intent intent = new Intent(this, AddTransactionActivity.class);
-            startActivity(intent);
+                binding.historyLayout.setBackgroundResource(R.drawable.round_back_history);
+                binding.historyIcon.setImageResource(R.drawable.history_selected_icon);
+                binding.historyText.setVisibility(View.VISIBLE);
+
+                binding.categoryLayout.setBackgroundResource(R.drawable.transparent_background);
+                binding.categoryIcon.setImageResource(R.drawable.categories_icon);
+                binding.categoryText.setVisibility(View.GONE);
+
+                binding.statisticLayout.setBackgroundResource(R.drawable.transparent_background);
+                binding.statisticIcon.setImageResource(R.drawable.statistic_icon);
+                binding.statisticText.setVisibility(View.GONE);
+
+//                ScaleAnimation scaleAnimation = new ScaleAnimation(0.8f, 1.0f, 1.0f, 1.0f, Animation.RELATIVE_TO_SELF, 0.0f, Animation.RELATIVE_TO_SELF, 0.0f);
+//                scaleAnimation.setDuration(200);
+//                scaleAnimation.setFillAfter(true);
+//                binding.historyLayout.startAnimation(scaleAnimation);
+
+                swapFragment(new TransactionsFragment());
+                chosenFragment = 1;
+            }
         });
+
+        binding.categoryLayout.setOnClickListener(view -> {
+            if (chosenFragment != 2) {
+
+                binding.historyLayout.setBackgroundResource(R.drawable.transparent_background);
+                binding.historyIcon.setImageResource(R.drawable.history_icon);
+                binding.historyText.setVisibility(View.GONE);
+
+                binding.categoryLayout.setBackgroundResource(R.drawable.round_back_category);
+                binding.categoryIcon.setImageResource(R.drawable.categories_selected_icon);
+                binding.categoryText.setVisibility(View.VISIBLE);
+
+                binding.statisticLayout.setBackgroundResource(R.drawable.transparent_background);
+                binding.statisticIcon.setImageResource(R.drawable.statistic_icon);
+                binding.statisticText.setVisibility(View.GONE);
+
+
+                swapFragment(new CategoryFragment());
+                chosenFragment = 2;
+            }
+        });
+
+        binding.statisticLayout.setOnClickListener(view -> {
+            if (chosenFragment != 3) {
+
+                binding.historyLayout.setBackgroundResource(R.drawable.transparent_background);
+                binding.historyIcon.setImageResource(R.drawable.history_icon);
+                binding.historyText.setVisibility(View.GONE);
+
+                binding.categoryLayout.setBackgroundResource(R.drawable.transparent_background);
+                binding.categoryIcon.setImageResource(R.drawable.categories_icon);
+                binding.categoryText.setVisibility(View.GONE);
+
+                binding.statisticLayout.setBackgroundResource(R.drawable.round_back_statistic);
+                binding.statisticIcon.setImageResource(R.drawable.statistic_selected_icon);
+                binding.statisticText.setVisibility(View.VISIBLE);
+
+
+                swapFragment(new StatisticFragment());
+                chosenFragment = 3;
+            }
+        });
+
+
     }
+
+
 
     private void swapFragment(Fragment fragment) {
         FragmentTransaction transaction = fragmentManager.beginTransaction();
